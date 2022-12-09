1ef764397311503cec6217784c8d9ad7fb367ed2
PrajaktaShewale <shewalep94@gmail.com>
2022-12-09 16:53:45 +0530

    [Prajakta]Add:Create Coupon Numbers problem solution

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -15,7 +15,7 @@ namespace LogicalPrograms
             {
                 Console.WriteLine("Logical Programming Problems");
                 Console.WriteLine("1.Fibonacci Series" + "\n" + "2.Perfect Number" + "\n" + "3.prime number"
-                    + "\n" + "4.Reverse Number" + "\n" + "5.Exit");
+                    + "\n" + "4.Reverse Number" + "\n" + "5.Coupon Number" + "\n" + "6.Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
@@ -35,6 +35,10 @@ namespace LogicalPrograms
                     case 4:
                         ReverseNumber rev = new ReverseNumber();
                         rev.Reverse();
+                        break;
+                    case 5:
+                        CouponNumbers coupo = new CouponNumbers();
+                        coupo.GenerateCoupons();
                         break;
                     default:
                         flag = false;
